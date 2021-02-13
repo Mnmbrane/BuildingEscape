21343bad4cbc05d9e575347105ae28f8fe8433e8
Mnmbrane <mnmbrane@gmail.com>
2021-02-12 23:41:52 -0500

    New materials and collision implemented.

---
+++ Source/Building_Escape/OpenDoor.cpp
@@ -0,0 +1,44 @@
+// Copyright Manny Candidato 2021
+
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	mTargetYaw = { 0, GetOwner()->GetActorRotation().Yaw + TARGET_YAW, 0 };
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	
+	UE_LOG(LogTemp, Warning, TEXT("Current Rotation: %s"), *GetOwner()->GetActorRotation().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("Yaw is: %f"), GetOwner()->GetActorRotation().Yaw);
+	UE_LOG(LogTemp, Warning, TEXT("Target Yaw: %f"), mTargetYaw.Yaw);
+
+	FRotator currentYaw = GetOwner()->GetActorRotation();
+	FRotator openDoor(0, 0, 0);
+
+	openDoor.Yaw = FMath::FInterpConstantTo(currentYaw.Yaw, mTargetYaw.Yaw, DeltaTime, 45);
+
+	GetOwner()->SetActorRotation(openDoor);
+}
+
